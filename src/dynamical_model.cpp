6b91d17a8c4eabc15da63f9498b4bd9e15f71f1d
dario <dario.zurlo@neura-robotics.com>
2022-12-21 11:58:22 +0100

    debug but stil nor working

--- src/dynamical_model.cpp
+++ src/dynamical_model.cpp
@@ -5,63 +5,64 @@ DynamicalModel::DynamicalModel(){
     //##########Dynamic Parameters #########//
     //masses
     m_.resize(DOFS);
-    m_ << 7.9637582, 2.6189549, 7.5440000, 2.3488000, 2.3432207, 8.2857161;
+     m_ << 7.9637582, 2.6189549, 7.5440000, 2.3488000, 2.3432207, 8.2857161;
+     //m_ << 7.9637582, 2.6189549, 7.5440000;
 
     //inertias
     inertia_.resize(DOFS);
-    // inertia_.at(0) << 0.047361000, 0, 0,
-    //                   0, 0.027948983, 0,
-    //                   0, 0, 0.040138000;
-    // inertia_.at(1) << 0.89171709, 0, 0,
-    //                   0, 1.7625421, 0,
-    //                   0, 0, 2.5169801;
-    // inertia_.at(2) << 0.66028304, 0, 0,
-    //                   0, 0.28351611, 0,
-    //                   0, 0, 0.93963656;
+    inertia_.at(0) << 0.047361000, 0, 0,
+                      0, 0.027948983, 0,
+                      0, 0, 0.040138000;
+    inertia_.at(1) << 0.89171709, 0, 0,
+                      0, 1.7625421, 0,
+                      0, 0, 2.5169801;
+    inertia_.at(2) << 0.66028304, 0, 0,
+                      0, 0.28351611, 0,
+                      0, 0, 0.93963656;
 
-    // inertia_.at(3) << 0.0, 0, 0,
-    //                   0, 0.36765057, 0,
-    //                   0, 0, 0.36765057;
+    inertia_.at(3) << 0.01, 0, 0,
+                      0, 0.36765057, 0,
+                      0, 0, 0.36765057;
 
-    // inertia_.at(4) << 0.0, 0, 0,
-    //                   0, 0.13644056, 0,
-    //                   0, 0, 0.0013644056;
+    inertia_.at(4) << 0.01, 0, 0,
+                      0, 0.13644056, 0,
+                      0, 0, 0.0013644056;
 
-    // inertia_.at(5) << 0.014304167, 0, 0,
-    //                   0, 0.010091649, 0,
-    //                   0, 0, 0.024395716;
+    inertia_.at(5) << 0.014304167, 0, 0,
+                      0, 0.010091649, 0,
+                      0, 0, 0.024395716;
 
-    inertia_.at(0) << 0.1, 0, 0,
-                      0, 0.1, 0,
-                      0, 0, 0.1;
-    inertia_.at(1) << 0.1, 0, 0,
-                      0, 0.1, 0,
-                      0, 0,0.1;
-    inertia_.at(2) << 0.1, 0, 0,
-                      0, 0.1, 0,
-                      0, 0, 0.1;
+    // inertia_.at(0) << 0.1, 0, 0,
+    //                   0, 0.1, 0,
+    //                   0, 0, 0.1;
+    // inertia_.at(1) << 0.1, 0, 0,
+    //                   0, 0.1, 0,
+    //                   0, 0,0.1;
+    // inertia_.at(2) << 0.1, 0, 0,
+    //                   0, 0.1, 0,
+    //                   0, 0, 0.1;
 
-    inertia_.at(3) << 0.1, 0, 0,
-                      0, 0.1, 0,
-                      0, 0, 0.1;
+    // inertia_.at(3) << 0.1, 0, 0,
+    //                   0, 0.1, 0,
+    //                   0, 0, 0.1;
 
-    inertia_.at(4) << 0.1, 0, 0,
-                      0, 0.1, 0,
-                      0, 0, 0.1;
+    // inertia_.at(4) << 0.1, 0, 0,
+    //                   0, 0.1, 0,
+    //                   0, 0, 0.1;
 
-    inertia_.at(5) << 0.1, 0, 0,
-                      0, 0.1, 0,
-                      0, 0, 0.1;
+    // inertia_.at(5) << 0.1, 0, 0,
+    //                   0, 0.1, 0,
+    //                   0, 0, 0.1;
 
 
     //cogs expressed wrt the next frame so ready to use in the formulas without transformation
     cog_.resize(DOFS);
     cog_.at(0) << -0.0023983783, -0.051099000, -0.0064075029;
-    cog_.at(1) << -0.027199000, -0.050000000, -0.080318000;
+    cog_.at(1) << -0.27199000, -0.050000000, -0.080318000;
     cog_.at(2) << 0.0062422845, 0.030640000, 0.063114058;
-    cog_.at(3) << 0.035455313, 0.021296500, -0.010879600;
+    cog_.at(3) << 0.035455313, 0.21296500, -0.10879600;
     cog_.at(4) << 0.0038853698, 0.025798751, 0.08656600;
-    cog_.at(5) << 0.0035370346, 0.0021101928, -0.025491905;
+    cog_.at(5) << 0.0035370346, 0.0021101928, -0.25491905;
 
 
     alpha_.resize(DOFS);
@@ -70,6 +71,9 @@ DynamicalModel::DynamicalModel(){
     alpha_ << M_PI/2, M_PI, M_PI/2, -M_PI/2, M_PI/2, 0.0;
     l_ << 0.0, 0.38, 0.0, 0.0, 0.0, 0.0;
     d_ << 0.22, 0.0, 0.0, 0.42, 0.0, 0.157;
+    // alpha_ << M_PI/2, M_PI, M_PI/2;
+    // l_ << 0.0, 0.38, 0.0;
+    // d_ << 0.22, 0.0, 0.0;
 
     //##########initialization velocities, accelleration#########//
     omega_.resize(DOFS+1);
@@ -109,27 +113,29 @@ void DynamicalModel::forwardRecursion(const Eigen::VectorXd &q, const Eigen::Vec
     std::vector<double> dh_parameters;
     dh_parameters.resize(4);
 
-
+   
     for(short int i=0; i<DOFS; i++){
 
-        dh_parameters.at(0) = q[i];
+        if(i==1 || i==2)
+            dh_parameters.at(0) = q[i]+M_PI/2;
+        else
+            dh_parameters.at(0) = q[i];
         dh_parameters.at(1) = d_[i];
         dh_parameters.at(2) = l_[i];
         dh_parameters.at(3) = alpha_[i];
 
-
        computeRotationTranslation(R, t, dh_parameters);
 
-
         omega_.at(i+1) = R.transpose()*(omega_.at(i) + dq[i]*z);
-
-        d_omega_.at(i+1) = R.transpose()*(d_omega_.at(i) + ddq[i]*z + (dq[i]*(omega_.at(i)).cross(z)));
+        d_omega_.at(i+1) = R.transpose()*(d_omega_.at(i) + ddq[i]*z + dq[i]*(omega_.at(i).cross(z)));
+        // std::cout << d_omega_.at(i+1) << std::endl;
+        //  std::cout  << std::endl;
 
         a_.at(i+1) = R.transpose()*a_.at(i) + d_omega_.at(i+1).cross(R.transpose()*t) + omega_.at(i+1).cross(omega_.at(i+1).cross(R.transpose()*t));
-
+        
         ac_.at(i) = a_.at(i+1) + d_omega_.at(i+1).cross(cog_.at(i)) + omega_.at(i+1).cross(omega_.at(i+1).cross(cog_.at(i)));
 
-        std::cout << "t --> " << R.transpose()*t << std::endl;
+       // std::cout << "t --> " << R.transpose()*t << std::endl;
         // std::cout << "omega_.at(i+1) --> " << omega_.at(i+1) << std::endl
         // << "d_omega_.at(i+1) --> " << d_omega_.at(i+1) << std::endl
         // << "a_.at(i+1) --> " << a_.at(i+1) << std::endl
@@ -152,6 +158,9 @@ void DynamicalModel::backwardRecursion(const Eigen::VectorXd &q){
     Eigen::Vector3d t;
     Eigen::Matrix3d Rp1;
 
+    R.setZero();
+    t.setZero();
+
     std::vector<double> dh_parameters;
     dh_parameters.resize(4);
 
@@ -161,28 +170,31 @@ void DynamicalModel::backwardRecursion(const Eigen::VectorXd &q){
            0, 1, 0,
            0, 0, 1;
 
-
+    std::cout  << tau_.at(0) << std::endl;
     for(short int i=DOFS-1; i>=0; i--){
 
         if(i==1 || i==2)
-            dh_parameters.at(0) = q[i]+M_PI;
+            dh_parameters.at(0) = q[i]+M_PI/2;
         else
             dh_parameters.at(0) = q[i];
         dh_parameters.at(1) = d_[i];
         dh_parameters.at(2) = l_[i];
         dh_parameters.at(3) = alpha_[i];
 
-
         computeRotationTranslation(R, t, dh_parameters);
 
        f_.at(i) = Rp1*f_.at(i+1) + m_[i]*(ac_.at(i));
+       
+       
 
-        tau_.at(i) = Rp1*tau_.at(i+1) + (Rp1*(f_.at(i+1))).cross(cog_.at(i)) - f_.at(i).cross(R.transpose()*t + cog_.at(i)) +
+        tau_.at(i) = Rp1*tau_.at(i+1) + (Rp1*f_.at(i+1)).cross(cog_.at(i)) - f_.at(i).cross(R.transpose()*t + cog_.at(i)) +
                     inertia_.at(i)*(d_omega_.at(i+1)) + omega_.at(i+1).cross(inertia_.at(i) * (omega_.at(i+1)));
 
-        u_[i] = tau_.at(i).transpose()*R.transpose()*z;
+        std::cout  << inertia_.at(i) << std::endl;
+        std::cout << std::endl;
 
-        computeRotationTranslation(Rp1, t, dh_parameters);
+        u_[i] = tau_.at(i).transpose()*R.transpose()*z;
+        Rp1 = R;
     }
 
 }
--- src/main.cpp
+++ src/main.cpp
@@ -7,56 +7,58 @@
 
 int main(int argc, char *argv[])
 {
+
+    const int DOFS = 6;
     //otherwise it returns always the same number
     srand((unsigned int) time(0));
 
-    DynamicalModel model_n;
+    // DynamicalModel model_n;
     Eigen::VectorXd q;
     Eigen::VectorXd dq;
     Eigen::VectorXd ddq;
 
-    q.resize(6);
-    dq.resize(6);
-    ddq.resize(6);
+    q.resize(DOFS);
+    dq.resize(DOFS);
+    ddq.resize(DOFS);
 
-
+    q <<  0, 0.50, 0.50, 0, 0, 0;
     //q.setRandom();
-    //q << 0, 0, 0.3, 0.4, 0.2, 0;
+   // q << 0, 0, 0.0, 0.0, 0.0, 0;
     //q << 0, 0, 0, 0, 0, 0;
-    q << 0, 0, 0, 0, 0, 0;
+    //q << 0, 0.50, 0.50;
 
 
-    Eigen::VectorXd n;
-    n.resize(6);
-    //dq.setRandom();
-    dq << 0,0,0,0,0,0;
-    ddq.setZero();
-    auto start = std::chrono::high_resolution_clock::now();
-    n = model_n.rnea(q, dq, ddq);
-    auto end = std::chrono::high_resolution_clock::now();
-    auto duration = std::chrono::duration_cast<std::chrono::microseconds>(end-start);
+    // Eigen::VectorXd n;
+    // n.resize(DOFS);
+    // //dq.setRandom();
+    // dq << 0,0,0,0,0,0;
+    // ddq.setZero();
+    // auto start = std::chrono::high_resolution_clock::now();
+    // n = model_n.rnea(q, dq, ddq);
+    // auto end = std::chrono::high_resolution_clock::now();
+    // auto duration = std::chrono::duration_cast<std::chrono::microseconds>(end-start);
 
-    Eigen::VectorXd g;
-    g.resize(6);
-    DynamicalModel model_g;
-    dq.setZero();
-    g = model_g.rnea(q, dq, ddq);
+    // Eigen::VectorXd g;
+    // g.resize(DOFS);
+    // DynamicalModel model_g;
+    // dq.setZero();
+    // g = model_g.rnea(q, dq, ddq);
 
     DynamicalModel model_M;
     Eigen::MatrixXd M;
-    M.resize(6,6);
+    M.resize(DOFS,DOFS);
     M.setZero();
 
     Eigen::VectorXd M_i;
-    M_i.resize(6);
+    M_i.resize(DOFS);
     M_i.setZero();
     dq.setZero();
     auto start_m = std::chrono::high_resolution_clock::now();
-    for(short int i=0; i<6; i++){
+    for(short int i=0; i<DOFS; i++){
         ddq.setZero();
         ddq[i] = 1;
         M_i = model_M.rnea( q, dq, ddq);
-        for(short int j=0; j<6; j++){
+        for(short int j=0; j<DOFS; j++){
             M(j,i) = M_i[j];
         }
         M_i.setZero();
@@ -64,12 +66,12 @@ int main(int argc, char *argv[])
     auto end_m = std::chrono::high_resolution_clock::now();
     auto duration_m = std::chrono::duration_cast<std::chrono::microseconds>(end_m-start_m);
 
-    std::cout << "--------------------n vector--------------------" << std::endl;
-    std::cout << n << std::endl;
-    std::cout << "Compute in " << duration.count() << std::endl;
+    // std::cout << "--------------------n vector--------------------" << std::endl;
+    // std::cout << n << std::endl;
+    // std::cout << "Compute in " << duration.count() << std::endl;
 
-    std::cout << "--------------------g vector--------------------" << std::endl;
-    std::cout << g << std::endl;
+    // std::cout << "--------------------g vector--------------------" << std::endl;
+    // std::cout << g << std::endl;
 
     std::cout << "--------------------M matrix--------------------" << std::endl;
     std::cout << M << std::endl;
